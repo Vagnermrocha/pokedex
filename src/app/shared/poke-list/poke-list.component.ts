e6f193b75649d27941c964e7908509aa0f0b4804
Vagnermrocha <vagnermendesrocha@gmail.com>
2023-10-25 17:34:19 -0300

    Atualizando o projeto

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from '../../service/poke-api.service';
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+export class PokeListComponent implements OnInit {
+
+  public getAllPokemons: any;
+
+  constructor(
+    private pokeApiService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokeApiService.appListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results;
+        console.log(this.getAllPokemons)
+      }
+    );
+  }
+
+}
