2abe59cd31c545554689ac01f5b0da8d49005db3
Vagnermrocha <vagnermendesrocha@gmail.com>
2023-10-26 18:24:05 -0300

    Projeto finalizado

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+//Services
+import { PokeApiService } from '../../service/poke-api.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  get getPokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(      //Faz os subscribe do pokemon e do name
+    res => {
+      this.pokemon = res;      // A resposta do serviço das duas urls que está puxando
+      this.isLoading = true;
+    },
+    error => {
+      this.apiError = true;
+    }
+    );
+  }
 
 }
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,7 +8,11 @@ import { PokeApiService } from '../../service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
 
+  private setAllPokemons: any;
   public getAllPokemons: any;
+
+  public apiError: boolean = false;
+
 
   constructor(
     private pokeApiService: PokeApiService
@@ -17,10 +21,22 @@ export class PokeListComponent implements OnInit {
   ngOnInit(): void {
     this.pokeApiService.appListAllPokemons.subscribe(
       res => {
+        this.setAllPokemons = res.results;
         this.getAllPokemons = res.results;
-        console.log(this.getAllPokemons)
+        //console.log(this.getAllPokemons)
+      },
+      error => {
+        this.apiError = true;
       }
     );
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 
 }
